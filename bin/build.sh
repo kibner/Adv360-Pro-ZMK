79de29aef8c185e3b4824dc1a6e876daa05be86c
John D'Oriocourt <kibner@gmail.com>
2023-01-11 14:38:58 +0000

    fixed issue with potentially not having the correct directories for the compilation databases created before trying to copy files to them

--- bin/build.sh
+++ bin/build.sh
@@ -16,4 +16,5 @@ cat build/right/zephyr/.config | grep -v "^#" | grep -v "^$"
 # Rename zmk.uf2
 cp build/left/zephyr/zmk.uf2 ./firmware/${TIMESTAMP}-left.uf2 && cp build/right/zephyr/zmk.uf2 ./firmware/${TIMESTAMP}-right.uf2
 # Copy compile_commands.json to host system
+mkdir -p ./compilation_databases/left && mkdir -p ./compilation_databases/right
 cp build/left/compile_commands.json ./compilation_databases/left/compile_commands.json && cp build/right/compile_commands.json ./compilation_databases/right/compile_commands.json
