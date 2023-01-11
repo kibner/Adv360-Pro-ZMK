7623ee8f01fb5a6b034fa482631a70835604f6f8
John D'Oriocourt <kibner@gmail.com>
2023-01-11 14:40:15 +0000

    made sure compilation directories are created and new versions of compile_commands.json overwrite old

--- bin/build.sh
+++ bin/build.sh
@@ -17,4 +17,4 @@ cat build/right/zephyr/.config | grep -v "^#" | grep -v "^$"
 cp build/left/zephyr/zmk.uf2 ./firmware/${TIMESTAMP}-left.uf2 && cp build/right/zephyr/zmk.uf2 ./firmware/${TIMESTAMP}-right.uf2
 # Copy compile_commands.json to host system
 mkdir -p ./compilation_databases/left && mkdir -p ./compilation_databases/right
-cp build/left/compile_commands.json ./compilation_databases/left/compile_commands.json && cp build/right/compile_commands.json ./compilation_databases/right/compile_commands.json
+cp -f build/left/compile_commands.json ./compilation_databases/left/compile_commands.json && cp -f build/right/compile_commands.json ./compilation_databases/right/compile_commands.json
